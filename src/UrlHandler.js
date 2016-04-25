bc7a1190c4a3af3853af571882c08c637504e851
Luis Felipe Pérez <xilenomg@gmail.com>
2016-04-25 13:24:52 -0300

    Url Handler

---
+++ src/UrlHandler.js
@@ -0,0 +1,130 @@
+var UrlHandler = function(url) {
+    this._protocol;
+    this._subdomain;
+    this._domain;
+    this._path;
+    this._query;
+    this._anchor;
+    this._originalURL;
+    this.setURL(url);
+    return this;
+}
+UrlHandler.prototype.reset = function() {
+    this._protocol = null;
+    this._subdomain = null;
+    this._domain = null;
+    this._path = null;
+    this._query = null;
+    this._anchor = null;
+    this._originalURL = null;
+};
+UrlHandler.prototype.setURL = function(url) {
+    UrlHandler.prototype.reset();
+
+    //["https://www.globo/path/to/folder?query=param#test", "https", "www", "globo", undefined, "/path/to/folder", "query=param", "test"]
+    //["https://www.globo.com/path/to/folder?query=param#test", "https", "www", "globo", "com", "/path/to/folder", "query=param", "test"]
+    var expression = /(.*):\/\/([^\.]+)\.([^\.\/\?\#]+)\.?([^\/\?\#]+)?([^\?\#]*)?\??([^#]*)?\#?(.*)?/;
+    var regex = new RegExp(expression);
+    var regexResult = url.match(regex);
+    if (regexResult) {
+        //Protocol
+        if (regexResult[1]) {
+            this.setProtocol(regexResult[1]);
+        } else {
+            this.setProtocol(null);
+        }
+
+        //subdomain
+        if (regexResult[2]) {
+            this.setSubdomain(regexResult[2]);
+        } else {
+            this.setSubdomain(null);
+        }
+
+        //domain
+        if (regexResult[3]) {
+            this.setDomain(regexResult[3] + (regexResult[4] ? '.' + regexResult[4] : ''));
+        } else {
+            this.setDomain(null);
+        }
+
+
+
+        //path
+        if (regexResult[5]) {
+            this.setPath(regexResult[5]);
+        } else {
+            this.setPath(null);
+        }
+
+        //query
+        if (regexResult[6]) {
+            this.setQuery(regexResult[6]);
+        } else {
+            this.setQuery(null);
+        }
+
+        //Anchor
+        if (regexResult[7]) {
+            this.setAnchor(regexResult[7]);
+        } else {
+            this.setAnchor(null);
+        }
+    }
+
+
+};
+
+UrlHandler.prototype.getProtocol = function() {
+    return this._protocol;
+};
+
+UrlHandler.prototype.getSubdomain = function() {
+    return this._subdomain;
+};
+
+UrlHandler.prototype.getDomain = function() {
+    return this._domain;
+};
+
+UrlHandler.prototype.getPath = function() {
+    return this._path;
+};
+
+UrlHandler.prototype.getQuery = function() {
+    return this._query;
+};
+
+UrlHandler.prototype.getAnchor = function() {
+    return this._anchor;
+};
+
+UrlHandler.prototype.setProtocol = function(protocol) {
+    this._protocol = protocol;
+};
+
+UrlHandler.prototype.setSubdomain = function(subdomain) {
+    this._subdomain = subdomain;
+};
+
+UrlHandler.prototype.setDomain = function(domain) {
+    this._domain = domain;
+};
+
+UrlHandler.prototype.setPath = function(path) {
+    this._path = path;
+};
+
+UrlHandler.prototype.setQuery = function(query) {
+    this._query = query;
+};
+
+UrlHandler.prototype.setAnchor = function(anchor) {
+    this._anchor = anchor;
+};
+
+UrlHandler.prototype.printURL = function() {
+    return this._protocol + '://' + this._subdomain + '.' + this._domain + '/' + this._path + '?'.this.query;
+};
+
+module.exports = UrlHandler;
---
+++ tests/url-test.js
@@ -0,0 +1,92 @@
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+var UrlHandler = require('./../src/UrlHandler');
+
+describe('UrlHandler', function() {
+    describe('getProtocol', function() {
+
+
+        it('getProtocol(http://www.domain.com) should return http', function() {
+            var url = new UrlHandler('http://www.domain.com');
+            expect(url.getProtocol()).to.equal('http');
+        });
+
+        it('getProtocol(https://www.domain.com) should return https', function() {
+            var url = new UrlHandler('https://www.domain.com');
+            expect(url.getProtocol()).to.equal('https');
+        });
+
+        it('getProtocol(https://www.domain) should return https', function() {
+            var url = new UrlHandler('https://www.domain');
+            expect(url.getProtocol()).to.equal('https');
+        });
+    })
+    describe('getSubdomain', function() {
+        it('getSubdomain(https://www.domain.com?query=test) should return www', function() {
+            var url = new UrlHandler('https://www.domain.com?query=test');
+            expect(url.getSubdomain()).to.equal('www');
+        });
+
+        it('getSubdomain(https://www.domain) should return www', function() {
+            var url = new UrlHandler('https://www.domain');
+            expect(url.getSubdomain()).to.equal('www');
+        });
+    });
+
+
+    describe('getDomain', function() {
+        it('getDomain(https://www.domain.com?query=test) should return domain.com', function() {
+            var url = new UrlHandler('https://www.domain.com?query=test');
+            expect(url.getDomain()).to.equal('domain.com');
+        });
+
+        it('getDomain(https://www.domain) should return domain', function() {
+            var url = new UrlHandler('https://www.domain');
+            expect(url.getDomain()).to.equal('domain');
+        });
+    });
+
+    describe('getPath', function() {
+        it('getPath(https://www.domain.com?query=test) should return null', function() {
+            var url = new UrlHandler('https://www.domain.com?query=test');
+            expect(url.getPath()).to.equal(null);
+        });
+
+        it('getPath(https://www.domain/path/to/folder) should return /path/to/folder', function() {
+            var url = new UrlHandler('https://www.domain/path/to/folder');
+            expect(url.getPath()).to.equal('/path/to/folder');
+        });
+
+        it('getPath(https://www.domain#test=test) should return null', function() {
+            var url = new UrlHandler('https://www.domain#test=test');
+            expect(url.getPath()).to.equal(null);
+        });
+
+        it('getPath(https://www.domain.com/path/to/folder?query=test#test=test) should return /path/to/folder', function() {
+            var url = new UrlHandler('https://www.domain.com/path/to/folder?query=test#test=test');
+            expect(url.getPath()).to.equal('/path/to/folder');
+        });
+    });
+
+    describe('getAnchor', function() {
+        it('getAnchor(https://www.domain.com?query=test) should return null', function() {
+            var url = new UrlHandler('https://www.domain.com?query=test');
+            expect(url.getAnchor()).to.equal(null);
+        });
+
+        it('getAnchor(https://www.domain/path/to/folder) should return /path/to/folder', function() {
+            var url = new UrlHandler('https://www.domain/path/to/folder');
+            expect(url.getAnchor()).to.equal(null);
+        });
+
+        it('getAnchor(https://www.domain#test=test) should return test=test', function() {
+            var url = new UrlHandler('https://www.domain#test=test');
+            expect(url.getAnchor()).to.equal('test=test');
+        });
+
+        it('getAnchor(https://www.domain.com/path/to/folder?query=test#test=test) should return /path/to/folder', function() {
+            var url = new UrlHandler('https://www.domain.com/path/to/folder?query=test#test=test');
+            expect(url.getAnchor()).to.equal('test=test');
+        });
+    });
+})
